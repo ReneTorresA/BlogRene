7f2be019f48c92b4b581f709bcc46e3ebf80d16e
ReneTorresA <rene.torres.a@usach.cl>
2022-08-30 02:47:26 -0400

    Se arregla la página de contacto

--- src/Pages/Contacto.jsx
+++ src/Pages/Contacto.jsx
@@ -3,24 +3,33 @@ import Footers from "../componentes/footer";
 
 const Contacto = () => {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className="row justify-content-center">
       <Navbar />
-      <div className="container text-center">
+      <div className="col-md-12 text-center">
         <h3>¿Te gustaría contactarme? </h3>
         <h3>Escríbeme aquí abajo 😸</h3>
+      </div>
+      <div className="col-md-12">
         <iframe
+          className="embed-responsive-item"
           src="https://docs.google.com/forms/d/e/1FAIpQLSfiY4MFvMxeeKlcrAdhaZWtQop9_qCqFI4249UV0TUKCFrg0w/viewform?embedded=true"
-          width="640"
-          height="500"
+          width="100%"
+          height="700"
           frameborder="0"
           marginheight="0"
           marginwidth="0"
+          allowfullscreen
         >
-          Cargando…
         </iframe>
+     
+        
       </div>
-    
+
+      <div className="row">
       <Footers />
+
+      </div>
+      
     </div>
   );
 };
