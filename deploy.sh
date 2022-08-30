bbcbae77660df070fbb3991c084e3c904098e667
ReneTorresA <rene.torres.a@usach.cl>
2022-08-30 04:47:59 -0400

    Se cambia el orden de las tarjetas de main

--- deploy.sh
+++ deploy.sh
@@ -49,9 +49,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:ReneTorresA/ReneTorresA.github.io.git main
+##git push -f git@github.com:ReneTorresA/ReneTorresA.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:ReneTorresA/blogrene.git main:gh-pages
+git push -f git@github.com:ReneTorresA/blogrene.git main:gh-pages
 
 cd -
