a3e59b877f8b14a9cf4977753cfb42745b7611b4
ReneTorresA <rene.torres.a@usach.cl>
2022-09-13 05:49:08 -0300

    Se agrega video- Ajuste de curvas MN

--- src/Pages/Metnumpage.jsx
+++ src/Pages/Metnumpage.jsx
@@ -8,11 +8,16 @@ import PlantillaCurso from "../componentes/plantillacursos";
 function Metnum() {
   const [show, setShow] = useState(false);
   const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
   const handleClose2 = () => setShow2(false);
   const handleShow2 = () => setShow2(true);
-  
+
+  const handleClose3 = () => setShow3(false);
+  const handleShow3 = () => setShow3(true);
   const itemsvideo = [
     {
       id: "0",
@@ -21,12 +26,17 @@ function Metnum() {
     },
 
     {
-      id:"1",
-      titulo:"Ayudantía 1: Repaso Matlab (Parte 2)",
+      id: "1",
+      titulo: "Ayudantía 1: Repaso Matlab (Parte 2)",
       urlvideo: "https://www.youtube.com/embed/-9VS9JfxJG8",
-    }
+    },
+    {
+      id: "2",
+      titulo: "Ayudantía 2: Ajuste de Curvas",
+      urlvideo: "https://www.youtube.com/embed/XR0VafgB_3M",
+    },
   ];
-  console.log(itemsvideo[0].urlvideo)
+  console.log(itemsvideo[0].urlvideo);
   const Metnuminfo = {
     id: "0",
     ruta: "/cursos",
@@ -110,6 +120,7 @@ function Metnum() {
           </>
         </div>
       </div>
+
       <div className="container col-lg-12">
         <div className="list-group">
           <>
@@ -150,7 +161,7 @@ function Metnum() {
           </>
         </div>
       </div>
-      
+
       <div className="container col-lg-12">
         <div className="list-group">
           <>
@@ -158,12 +169,40 @@ function Metnum() {
               type="button"
               className="btn btn-dark mb-1"
               variant="secondary btn-outline-light"
+              onClick={handleShow3}
             >
-              {'Ajuste de curvas (próximamente)'}
+              {itemsvideo[2].titulo}
             </button>
+
+            <Modal size="xl" centered show={show3} onHide={handleClose3}>
+              <Modal.Header closeButton></Modal.Header>
+              <Modal.Body>
+                <Container>
+                  <div className="container-fluid text-center">
+                    <div className="embed-responsive embed-responsive-16by9">
+                      <iframe
+                        className="embed-responsive-item videos-modales"
+                        src={itemsvideo[2].urlvideo}
+                        allowFullScreen
+                      ></iframe>
+                    </div>
+                  </div>
+                </Container>
+              </Modal.Body>
+
+              <Modal.Footer>
+                <Button
+                  variant="secondary btn-outline-light"
+                  onClick={handleClose3}
+                >
+                  Cerrar
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </>
         </div>
       </div>
+
       <Footers />
     </>
   );
